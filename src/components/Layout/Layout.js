eca19461e599e734c70669f4ba16dbed5b271664
Winter2016 <andrey.chirkov@zedconnect.com>
2019-01-24 22:12:07 +0300

    Initial burger creation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Aux from '../../hoc/Aux'
-import App from '../../App';
+import Aux from '../../hoc/Auxl';
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
